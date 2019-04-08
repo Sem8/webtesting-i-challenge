3179aa46fce1ca0715ea4e7f49a9d565006499c2
Sem8 <mssemmi8@gmail.com>
2019-04-08 18:32:28 -0400

    MVP without the edge cases

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -9,12 +9,29 @@ function succeed(item) {
   const newItem = { ...item };
   if (newItem.enhancement < 20) {
     newItem.enhancement++;
-  }   
+  }
   return newItem;
 }
 
 function fail(item) {
-  return { ...item };
+  const newItem = { ...item };  
+  
+    if (newItem.enhancement < 15) {      
+      if(newItem.durability >= 0) {
+        newItem.durability -= 5;
+      } else {
+        newItem.durability == 0;
+      }           
+    };  
+
+    if (newItem.enhancement >= 15) {
+      newItem.durability -= 10;
+    }  
+ 
+  if (newItem.enhancement > 16) {
+    newItem.enhancement--;
+  }
+  return newItem;
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -28,6 +28,43 @@ describe('enhancer', () => {
             };
             expect(succeed(item2).enhancement).toBe(20); 
             expect(succeed(item2).durability).toBe(1);           
-        });
+        });       
     });
+    describe('fail', () => {        
+        it('should decrease durability by 5 if enhancement is less than 15', () => {
+            const item3 = {
+                name: 'Lambda',
+                durability: 19,
+                enhancement: 14,
+            };
+            expect(fail(item3).durability).toBe(14);
+            expect(fail(item3).enhancement).toBe(14);
+        });
+        it('should decrease durability by 10 if enhancement is 15 or more', () => {
+            const item4 = {
+                name: 'Lambda',
+                durability: 19,
+                enhancement: 15,
+            };
+            expect(fail(item4).durability).toBe(9);
+            expect(fail(item4).enhancement).toBe(15);
+        });
+        // it('should decrease durabiltiy by 10 but NOT less than 0 if enhancement is 15 or more', () => {
+        //     const item = {
+        //         name: 'Lambda',
+        //         durability: 9,
+        //         enhancement: 15,
+        //     };
+        //     expect(fail(item).durability).toBe(0);
+        //     expect(fail(item).enhancement).toBe(15);
+        // });
+        it('should decrease durabiltiy by 10 but NOT less than 0 if enhancement is 15 or more', () => {
+            const item = {
+                name: 'Lambda',
+                durability: 9,
+                enhancement: 17,
+            };            
+            expect(fail(item).enhancement).toBe(16);
+        });
+    })
 })
