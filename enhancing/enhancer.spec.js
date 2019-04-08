4c13c849502a4d9ca68838100e0f7df380bb8551
Sem8 <mssemmi8@gmail.com>
2019-04-08 17:08:51 -0400

    all succeed function tests passed

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -4,14 +4,30 @@ const { repair, succeed, fail } = require('./enhancer.js');
 // test away!
 
 describe('enhancer', () => {
+    const item = {
+        name: 'Lambda',
+        durability: 1,
+        enhancement: 2,
+    };
     describe('repair', () => {
-        it('should repair durability to 100', () => {
-            const item = {
+        it('should repair durability to 100', () => {            
+            expect(repair(item).durability).toBe(100);
+        });
+    });
+    describe('succeed', () => {
+        it('should increase enhancement by 1 if less than 20', () => {
+            expect(succeed(item).enhancement).toBe(3);
+            expect(succeed(item).durability).toBe(1);
+        });
+
+        it('should not increase enhancement by 1 if already 20 or more', () => {
+            const item2 = {
                 name: 'Lambda',
                 durability: 1,
-                ehancement: 2,
+                enhancement: 20,
             };
-            expect(repair(item).durability).toBe(100);
-        })
-    })
+            expect(succeed(item2).enhancement).toBe(20); 
+            expect(succeed(item2).durability).toBe(1);           
+        });
+    });
 })
